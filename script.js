936948621fd03abb4268761a0a867bd1bcd1d915
Olivia <ohendrick1223@gmail.com>
2016-10-30 17:17:30 -0600

    footer,nav,api call for quotes

---
+++ script.js
@@ -0,0 +1,24 @@
+(function () {
+"use strict";
+
+
+var inputQuery='http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
+
+console.log(inputQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}) ();

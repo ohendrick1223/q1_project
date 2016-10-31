60cec1ba26863a7a7d3aec995ffb8f2f10093963
Olivia <ohendrick1223@gmail.com>
2016-10-30 19:00:43 -0600

    got POD api returning in console, added more skeleton to home page

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 (function () {
 "use strict";
 
+// append data of APOD to card with class of .apod
+var quotesQuery='http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
 
-var inputQuery='http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
-
-console.log(inputQuery);
+console.log(quotesQuery);
 
 
+var podQuery = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
 
+console.log(podQuery);
 
 
 

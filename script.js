9d280abf4a5c2fed7faf21d618e30a33d47a3656
Olivia <ohendrick1223@gmail.com>
2016-10-31 09:51:49 -0600

    have quote/author on the page!!!!!

--- script.js
+++ script.js
@@ -3,29 +3,39 @@
 
         // append data of APOD to card with class of .apod
         var quotesQuery = 'http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
-
         // console.log(quotesQuery);
-
-
         var podQuery = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
-        //
-        // console.log(podQuery);
-
+        //console.log(podQuery);
         var $xhr = $.getJSON(quotesQuery);
-        // var author = $xhr.Object;
-        // console.log(author);
-        console.log($xhr);
         $xhr.done(function(data) {
                 if ($xhr.status !== 200) {
                     console.log("there was an error")
                     return;
                 } else {
-                    // console.log(data.contents.author);
+                  //console.log(data);
                     var author = data.contents.author;
-                    console.log(author);
-                     $('#author').append(author);
+                    var quote = data.contents.quote;
+                    // console.log(quote);
+                    // console.log(author);
+                    $("#quote").html("<p>" + quote + "</p>");
+                    $("#author").html("<p>" + author + "</p>");
                 }
             });
+
+            var $xhr = $.getJSON(podQuery);
+            $xhr.done(function(data) {
+                    if ($xhr.status !== 200) {
+                        console.log("there was an error")
+                        return;
+                    } else {
+                      var img = data.url;
+                      //console.log(img);
+
+                        //append URL of IMG to CARD.
+                    }
+                });
+
+
 
 
 

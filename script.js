490e022c0b5f056e11b2e163df53291102ec0dc3
Olivia <ohendrick1223@gmail.com>
2016-10-30 20:35:55 -0600

    quotes/pod api returning traversible objects in DOM

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-(function () {
-"use strict";
+(function() {
+        "use strict";
 
-// append data of APOD to card with class of .apod
-var quotesQuery='http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
+        // append data of APOD to card with class of .apod
+        var quotesQuery = 'http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
 
-console.log(quotesQuery);
-
-
-var podQuery = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
-
-console.log(podQuery);
+        // console.log(quotesQuery);
 
 
+        var podQuery = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+        //
+        // console.log(podQuery);
+
+        var $xhr = $.getJSON(quotesQuery);
+        // var author = $xhr.Object;
+        // console.log(author);
+        console.log($xhr);
+        $xhr.done(function(data) {
+                if ($xhr.status !== 200) {
+                    console.log("there was an error")
+                    return;
+                } else {
+                    // console.log(data.contents.author);
+                    var author = data.contents.author;
+                    console.log(author);
+                     $('#author').append(author);
+                }
+            });
 
 
 
@@ -21,6 +35,4 @@ console.log(podQuery);
 
 
 
-
-
-}) ();
+        })();

a4a917d320cbc0b281240a9dfe828e260d80acb6
Olivia <ohendrick1223@gmail.com>
2016-11-01 17:47:30 -0600

    adding APOD object data dynamically to page!, working with img slider

--- script.js
+++ script.js
@@ -1,64 +1,57 @@
 (function() {
-        "use strict";
+    "use strict";
+$('.slider').slider({full_width: true, height: '800px'});
+$('.slider').slider('next');
 
-        var quotesQuery = 'http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
-        // console.log(quotesQuery);
-        var podQuery = 'https://api.nasa.gov/planetary/apod?api_key=omVNer26rAfPOkqm3gxZvnjwPJxr6OACKPBuh34M';
-        //console.log(podQuery);
-        //
-        // var twitterQuery =
+    var quotesQuery = 'http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
+    var picQuery = 'https://api.nasa.gov/planetary/apod?api_key=omVNer26rAfPOkqm3gxZvnjwPJxr6OACKPBuh34M';
 
+    $.getJSON(quotesQuery)
+        .done(function(data) {
+            if (data.error) {
+                console.log("there was an error");
+            } else {
+                var author = data.contents.author;
+                var quote = data.contents.quote;
+                $("#quoteDiv").html("<p>" + "<i class='medium material-icons'>format_quote</i>" + quote + "</p>");
+                $("#authorDiv").html("<p>" + author + "</p>");
+            }
+        });
 
-        var quotesObject = $.getJSON(quotesQuery);
-        quotesObject.done(function(data) {
-                if (quotesObject.status !== 200) {
-                    console.log("there was an error");
-                    return;
-                } else {
-                    var author = data.contents.author;
-                    var quote = data.contents.quote;
-                    // console.log(quote);
-                    // console.log(author);
-                    // console.log(data);
-                    $("#quoteDiv").html("<p>" + "<i class='medium material-icons'>format_quote</i>" + quote + "</p>");
-                    $("#authorDiv").html("<p>" + author + "</p>");
+    $.getJSON(picQuery)
+        .done(function(data) {
+            //console.log(data);
+            if (data.error) {
+                console.log('there was an error');
+            } else {
+                var date = moment(data.date);
+                for (var i = 0; i < 8; i++) {
+                    //console.log(date);
+                    var DataForPage = 'https://api.nasa.gov/planetary/apod?api_key=lWSexX3DkOyuXuh0V67U54MI7UAuFEyySVEFFJkz&date=' + date.subtract(1, 'days').format('YYYY-M-DD');
+                    //queries url (which is equal to current day and previous seven dates/metadata associated.)
+                    $.getJSON(DataForPage).done(renderImage);
                 }
-            });
-
-            $.getJSON(podQuery)
-              .done(function(data) {
-                //console.log(data);
-                if(data.error){
-                  console.log('there was an error');
-                } else {
-
+            }
+        });
+    function renderImage(data) {
                   var title = data.title;
                   var pic = data.url;
                   var content = data.explanation;
                   var date = moment(data.date);
+        $('#results')
+            .append($('<div>')
+                .append($('<img>').attr('src',pic).addClass('image-override col s12')
+
+                .append($("<div>").html("<p>" + moment(date).format('MMMM Do YYYY') + "</p>").addClass('center-align white-text dateDiv dateBox')))
+
+                .append($('<div>').html("<p class='title'>" + title).addClass('col s12 flow-text valign-wrapper center-align titleFont'))
+
+                .append($('<div>').html("<p class='card-content contentFont'>" + content + "</p>"))
+
+            );
 
 
-                  // console.log(date.format('MMMM Do YYYY'));
-
-                  $("#picDiv").attr('src', pic);
-                  $("#titleDiv").html("<p class='center-align title'>" + title + "</p>");
-                  $("#contentDiv").html("<p>" + content + "</p>");
-                  $("#dateDiv").html("<p class='center-align' id='dateBox'>" + date.format('MMMM Do YYYY') + "</p>");
-                }
+    }
 
 
-                });
-
-
-
-
-
-
-
-
-
-
-
-
-
-        })();
+})();

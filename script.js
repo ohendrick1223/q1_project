1d8cba422d16696496a93edf64349aa05b9775b6
Olivia <ohendrick1223@gmail.com>
2016-11-02 22:15:45 -0600

    dynamically loading 250 UFO <td>s, changed header,quote,background styling

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 (function() {
     "use strict";
-$('.parallax').parallax();
+
+    // $.ajaxSetup({cache:false});
+
+    $('.back-to-top').hide(); //bug fix for b2top btn
+
+    $('.parallax').parallax(); //initialize parallax
+
     var quotesQuery = 'http://quotes.rest/quote.json?maxlength=100&author=carl%20sagan&api_key=ZkXJZcqV_BbAAwTS_A93NAeF';
     var picQuery = 'https://api.nasa.gov/planetary/apod?api_key=omVNer26rAfPOkqm3gxZvnjwPJxr6OACKPBuh34M';
+    var ufoQuery = 'https://www.quandl.com/api/v3/datasets/NUFORC/SIGHTINGS.json?api_key=xAux-zbcJXrBiFy32jqC&start_date=1996-01-01';
 
+// =============quote request===========
     $.getJSON(quotesQuery)
         .done(function(data) {
             if (data.error) {
                 console.log("there was an error");
             } else {
+              // console.log(data);
                 var author = data.contents.author;
                 var quote = data.contents.quote;
                 $("#quoteDiv").html("<p>" + "<i class='medium material-icons'>format_quote</i>" + quote + "<i class='medium material-icons'>format_quote</i>" + "</p>");
@@ -16,6 +25,9 @@ $('.parallax').parallax();
             }
         });
 
+    // $.ajaxSetup({cache:true});
+
+// ================pic of day request========
     $.getJSON(picQuery)
         .done(function(data) {
             //console.log(data);
@@ -24,10 +36,14 @@ $('.parallax').parallax();
                 console.log('there was an error');
             } else {
                 var date = moment(data.date);
+                // console.log(date);
                 for (var i = 0; i < 30; i++) {
-                    var DataForPage = 'https://api.nasa.gov/planetary/apod?api_key=lWSexX3DkOyuXuh0V67U54MI7UAuFEyySVEFFJkz&date=' + date.format('YYYY-M-DD');
+                    var DataForPage = 'https://api.nasa.gov/planetary/apod?api_key=lWSexX3DkOyuXuh0V67U54MI7UAuFEyySVEFFJkz&date=' + date.subtract(1, 'days').format('YYYY-M-DD');
 
-                    date.subtract(1, 'days')
+                    //This formatting creates a 500 server-side error, because .moment(date) method updates a good 6 hours before it's actually updating, and thus can't find a url for "tomorrow".
+                    //  + date.format('YYYY-M-DD');
+                    // date.subtract(1, 'days')
+
                         //queries url (which is equal to current day and previous seven dates/metadata associated.)
                     allImages.push($.getJSON(DataForPage));
                 }
@@ -36,10 +52,11 @@ $('.parallax').parallax();
                         renderImage(results[i]);
                     }
                     $('.materialboxed').materialbox();
-                })
+                });
             }
         });
-
+// ============dynamically adding imgs to html=======
+    //
     function renderImage(data) {
         var title = data.title;
         var pic = data.url;
@@ -50,26 +67,22 @@ $('.parallax').parallax();
             // console.log('error', $(this));
             $(this).attr('src', 'img/no-image-placeholder.png');
         });
-        $img.attr('src', pic).addClass('image-override col s12 materialboxed z-depth-5')
+        $img.attr('src', pic).addClass('image-override col s12 materialboxed z-depth-5');
         $('#results')
             // .append($('<li>')
             .append($('<div>')
-
                 .append($img)
 
                 .append($("<div>").html("<p class='dateDiv dateBox'>" + moment(date).format('MMMM Do YYYY') + "</p>").addClass('center-align white-text'))
 
-
                 .append($("<div>").html("<p class='title'>" + title + "</p>").addClass('col s12 flow-text valign-wrapper center-align titleFont'))
-
 
                 .append($('<div>').html("<p>" + content + "</p>").addClass('card-content contentFont flow-text section'))
             );
     }
-
+// ==============back to top button========
     var offset = 3000;
     var duration = 300;
-
     $(window).scroll(function() {
         if ($(this).scrollTop() > offset) {
             $('.back-to-top').fadeIn(duration);
@@ -77,15 +90,41 @@ $('.parallax').parallax();
             $('.back-to-top').fadeOut(duration);
         }
     });
-
     $('.back-to-top').click(function(event) {
         event.preventDefault();
-
         $('html, body').animate({
             scrollTop: 0
         }, duration);
         return false;
     });
 
+// ========UFO request=============
+    $.getJSON(ufoQuery)
+      .done(function(data) {
+        var allMonths = [];
+        var allCounts = [];
+        //console.log(data.dataset.data);
+        if (data.error) {
+            console.log('there was an error');
+        } else {
+          var ufoData = data.dataset.data;
+          for (var prop in ufoData) {
+            //console.log(ufoData[prop][0]);
+            //console.log(ufoData[prop][1]);
+            allMonths.push(ufoData[prop][0]);
+            allCounts.push(ufoData[prop][1]);
+          }
+          var renderUfo = function(data) {
+            for (var i=0; i < allMonths.length; i++) {
+              for (var c=0; c < allCounts.length; c++) {
+                $("#tbody")
+                  .append($("<tr>").html("<td>" + allMonths[i] + "</td>" + "<td>" + allCounts[c] + "</td>"));
+                  }
+                }
+              };renderUfo(data);
+// console.log(allMonths);
+// console.log(allCounts);
+          }
+      });
 
 })();
